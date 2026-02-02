e04f14780b7b4197ab1aee657f0601d0eae0c57a
ujamstudio <ujamstudio@gmail.com>
2026-02-02 11:45:27 +0900

    completed proj0 ~task4

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -1,13 +1,17 @@
 package game2048logic;
 
 import game2048rendering.Side;
-import static game2048logic.MatrixUtils.rotateLeft;
-import static game2048logic.MatrixUtils.rotateRight;
+import net.sf.saxon.functions.ConstantFunction;
+import net.sf.saxon.functions.Minimax;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author  Josh Hug
  */
 public class GameLogic {
+    private static final Logger log = LoggerFactory.getLogger(GameLogic.class);
+
     /** Moves the given tile up as far as possible, subject to the minR constraint.
      *
      * @param board the current state of the board
@@ -20,7 +24,44 @@ public class GameLogic {
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
         // TODO: Fill this in in tasks 2, 3, 4
+        int origin_v = board[r][c];
+        int search_row =r;
+        int target_row;
+
+        //search possible position
+        while (search_row !=0){
+            search_row --;
+            if (board[search_row][c] != 0){
+                break;
+            }
+        }
+//set a target
+        if (search_row >=minR){
+             target_row = search_row;
+        } else {
+            target_row = minR;
+        }
+
+
+        System.out.println(target_row);
+        while (target_row != r) {
+            if (board[target_row][c]==origin_v) {
+                board[target_row][c] *= 2;
+                board[r][c] = 0;
+                return 1 + target_row;
+            }else {
+                if (board[target_row][c]==0){
+                    board[target_row][c] = origin_v;
+                    board[r][c]= 0;
+                    return 0;
+                }
+                else {
+                    target_row ++;
+                }
+            }
+        }
         return 0;
+
     }
 
     /**
